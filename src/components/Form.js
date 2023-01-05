0753076b6f2bd582e4fa392c21c9dc9dcc42e58a
Juan Roma <romajuanmanuel@gmail.com>
2023-01-04 22:34:51 -0300

    ??

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,11 +3,14 @@ import React, {useState} from 'react';
 function Form(){
 
     const [page, setPage] = useState(0);
+    const FormTitles = ["Sing Up", "Personal Info", "Oter"]
     return(
         <div cassName="form">
             <div className="progressbar"></div>
             <div className="form-container"></div>
-                <div className="header"></div>
+                <div className="header">
+                    <h1>{FormTitles[page]}</h1>
+                </div>
                 <div className="body"></div>
                 <div className="footer"></div>
                 <button>Prev</button>
