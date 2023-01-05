6f84848341dac443e940fc6c6e0670cc9c7e8805
Juan Roma <romajuanmanuel@gmail.com>
2023-01-05 19:11:32 -0300

    Redyy

--- src/components/SignUpInfo.js
+++ src/components/SignUpInfo.js
@@ -3,20 +3,21 @@ import React from "react";
 function SignUpInfo({ formData, setFormData }) {
   return (
     <div className="sign-up-container">
+     
       <input
         type="text"
-        placeholder="Email..."
-        value={formData.email}
-        onChange={(event) =>
-          setFormData({ ...formData, email: event.target.value })
-        }
-      />
-      <input
-        type="text"
-        placeholder="Password..."
+        placeholder="Full Name"
         value={formData.password}
         onChange={(event) =>
           setFormData({ ...formData, password: event.target.value })
+        }
+      />
+       <input
+        type="text"
+        placeholder="Email Adress"
+        value={formData.email}
+        onChange={(event) =>
+          setFormData({ ...formData, email: event.target.value })
         }
       />
       <input
