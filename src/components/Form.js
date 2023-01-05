38a44decabd5f4341aaba4554d7fd984f985645e
Juan Roma <romajuanmanuel@gmail.com>
2023-01-04 22:07:19 -0300

    Hi

---
+++ src/components/Form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Form(){
+    return(
+        <div cassName="form">
+            <div className="progressbar"></div>
+            <div className="form-container"></div>
+                <div className="header"></div>
+                <div className="body"></div>
+                <div className="footer"></div>
+                <button>Prev</button>
+                <button>Next</button>
+        </div>
+    )
+}
+
+export default Form;
