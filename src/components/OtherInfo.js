69aa2cb4675e21c5fe899ae66bbf1a7465f71bfe
Juan Roma <romajuanmanuel@gmail.com>
2023-01-05 19:01:30 -0300

    redy

--- src/components/OtherInfo.js
+++ src/components/OtherInfo.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
-function OtherInfo() {
+function OtherInfo({ formData, setFormData }) {
   return (
-    <div>OtherInfo</div>
-  )
+    <div className="other-info-container">
+      <input
+        type="text"
+        placeholder="Nationality..."
+        value={formData.nationality}
+        onChange={(e) => {
+          setFormData({ ...formData, nationality: e.target.value });
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Other..."
+        value={formData.other}
+        onChange={(e) => {
+          setFormData({ ...formData, other: e.target.value });
+        }}
+      />
+    </div>
+  );
 }
 
-export default OtherInfo
+export default OtherInfo;
